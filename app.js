f18b854009eead406b33268e0e3d78cbb22f4976
Greg Janco <gjanco@gmail.com>
2022-11-10 17:05:20 -0500

    change to port 80

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 
 http.createServer(function(request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
